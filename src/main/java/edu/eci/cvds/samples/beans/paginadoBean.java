070c9f781e7dd0ff3e27d712e576ae80bcaa63d0
Mateo Quintero <Mateo.quintero@mail.escuelaing.edu.co>
2020-04-23 20:23:10 -0500

    backend funcionalidad 8

---
+++ src/main/java/edu/eci/cvds/samples/beans/paginadoBean.java
@@ -0,0 +1,48 @@
+package edu.eci.cvds.samples.beans;
+
+
+import edu.eci.cvds.samples.entities.Area;
+import edu.eci.cvds.samples.entities.Idea;
+import edu.eci.cvds.samples.services.ServicioBancodeProyectos;
+import org.primefaces.model.chart.PieChartModel;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.List;
+
+@ManagedBean(name = "estadisticaBean")
+@SessionScoped
+
+
+public class paginadoBean extends BaseBean {
+   @Inject
+   private ServicioBancodeProyectos servicio;
+   private List<Idea> ideas;
+
+    private void generar() {
+        ideas = servicio.consultarTodo();
+    }
+    public void volver() {
+        ideas = null;
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("usuario.xhtml");
+        } catch (IOException e) {
+        }
+    }
+    public List<Idea> getIdeas() {
+        return ideas;
+    }
+
+    public void setIdeas(List<Idea> ideas) {
+        this.ideas = ideas;
+    }
+
+    @PostConstruct
+    public void init() {
+        super.init();
+        generar();
+    }
+}
